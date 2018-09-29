75a0b52973198914e1272bb30940ca389ad00256
Jonathan Hong <jh1294@ilab1.cs.rutgers.edu>
2018-09-29 16:51:41 -0400

    Fix read from stdin functionality in main.c

--- asst0/main.c
+++ asst0/main.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
 int main(int argc, char* argv[]){
+  // create file pointer
   FILE* fpointer;
+  // file pointer reads from stdin
   fpointer = stdin;
+  
+  
   char* str;
-  printf("Argument count is %d\n", argc);
-  while(scanf("%[^ ]%*s", str) != EOF){
+
+  // reads from stdin until end of file
+  while(scanf("%[^\n]%*c", str) != EOF){
     printf("Output: %s\n", str);
   } 
+
  return 0;
 }
