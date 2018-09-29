bb8eff22a140cf40decb284ecaf5dcee2db4869b
Jonathan Hong <jh1294@ilab2.cs.rutgers.edu>
2018-09-29 18:18:08 -0400

    Fix read from stdin to use fgets

--- asst0/main.c
+++ asst0/main.c
@@ -1,17 +1,27 @@
 #include <stdio.h>
+
 int main(int argc, char* argv[]){
   // create file pointer
   FILE* fpointer;
   // file pointer reads from stdin
   fpointer = stdin;
   
+  char str[512]; 
+
+  if (argc != 3){
+    printf("Incorrect number of arguments");
+    return -1;
+  } else if (strcmp(argv[1], "-c") != 0){
+      printf("Missing -c argument.\n");
+    return -1;
+  }
+  char str[512];
+  fgets(str, sizeof str, fpointer);
   
-  char* str;
-
   // reads from stdin until end of file
-  while(scanf("%[^\n]%*c", str) != EOF){
-    printf("Output: %s\n", str);
-  } 
-
+  while(fgets(str, sizeof str, fpointer) != NULL){
+   printf("Output: %s\n", str);
+  }
+   
  return 0;
 }
