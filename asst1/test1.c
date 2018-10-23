b91a7e0488c049971205a64e43d07d8ecb92edc4
Jonathan Hong <jh1294@ilab1.cs.rutgers.edu>
2018-10-23 00:09:54 -0400

    Fix test1.c to check for flags

--- asst1/test1.c
+++ asst1/test1.c
@@ -8,14 +8,19 @@ int main(int argc, char* argv[]){
   int cflag = 0;
   int dflag = 0;
   int oflag = 0;
-  char* column, inputDir, outputDir; 
+  char* column = "";
+  char* inputDir = "";
+  char* outputDir = ""; 
+  if(argc != 7){
+    //error: incorrect number of arguments
+  }
   for(i = 0; i < argc; i++){
-    if(strcmp(argv[i], "-c") == 0){
+    if(cflag == 0 && strcmp(argv[i], "-c") == 0){
       cflag = 1;
       continue;
     }
-    if(cflag == 1 && column == NULL){
-      column = (char *)malloc(strlen(argv[i])*sizeof(char));
+    if(cflag == 1 && strlen(column) == 0){
+      column = malloc((strlen(argv[i])+1)*sizeof(char));
       strcpy(column, argv[i]);
       continue;
     }
@@ -23,22 +28,25 @@ int main(int argc, char* argv[]){
       dflag = 1;
       continue;
     }
-    if(dflag == 1 && inputDir == NULL){
-      column = (char *)malloc(strlen(argv[i])*sizeof(char));
+    if(dflag == 1 && strlen(inputDir) == 0){
+      inputDir = malloc((strlen(argv[i])+1)*sizeof(char));
       strcpy(inputDir, argv[i]);
       continue;
     }
-    if(strcmp(argv[i],"-o") == 0){
+    if(oflag == 0 && strcmp(argv[i],"-o") == 0){
       oflag = 1;
       continue;
     }
-	if(oflag == 1 && outputDir == NULL){
-      column = (char *)malloc(strlen(argv[i])*sizeof(char));
+    if(oflag == 1 && strlen(outputDir) == 0){
+      outputDir = malloc((strlen(argv[i])+1)*sizeof(char));
       strcpy(outputDir, argv[i]);
       continue;
     }
   }
   printf("cflag = %d ; dflag = %d ; oflag = %d\n", cflag, dflag, oflag);
   printf("column = %s ; inputDir = %s ; outputDir = %s \n", column, inputDir, outputDir); 
+  free(inputDir);
+  free(outputDir);
+  free(column);
   return 0;
 }
