8f9ee9ae81dbc07a478b6495ec588ee30bc8701d
Valank <bym7@cd.cs.rutgers.edu>
2019-03-05 23:57:14 -0500

    something

---
+++ 1/mymalloc.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+struct data //metadata, first is the first two digits, second is last two digits of how much space the following 
+{
+	char first;
+	char second;
+};
+
+char * myMalloc(int size){
+	
+}
