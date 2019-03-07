46e5829589709de4bfb9ee8b5ed034de7137ee92
Valank <bym7@cd.cs.rutgers.edu>
2019-03-07 00:34:28 -0500

    fahwslfjsdl

--- 1/mymalloc.c
+++ 1/mymalloc.c
@@ -5,35 +5,72 @@
 
 static char myblock[4096];
 
-struct data //metadata, first is the first two digits, second is last two digits of how much space the following 
+struct data //metadata, first is the first two digits, second is last two digits of how much space the following block takes up
 {
-	char first;
-	char second;
-	int test;
+	char Num1;
+	char Num2;
+	char Size1;
+	char Size2;
 };
 
+void initialize(){
+	struct data meta;
+	meta.Num1 = 'A';
+	meta.Num2 = 'B';
+	meta.Size1 = convertToFirstChar(4096);
+	meta.Size2 = convertToSecondChar(4096);
+	void * ptr = &myblock[0];
+	*(struct data*)ptr = meta;
+}
+
 int compMagic(char first, char second){
-	if(first == 'A' && second == 'B')
+	if((first == 'A' && second == 'B') || (first == 'C' && second == 'D'))
 	{
 		return 1;
 	}
 	return 0;
 }
 
-char * myMalloc(int size){
+int convertToSize(char first, char second){
+	int x = first;
+	int y = second;
+	return ((x * 100) + y);
+}
+
+char convertToFirstChar(int num){
+	int x = num / 100;
+	char c = (char)x;
+	return c;
+}
+
+char convertToSecondChar(int num){
+	int x = num % 100;
+	char c = (char)x;
+	return c;
+}
+
+void * myMalloc(int size){
 	if(!compMagic(myblock[0], myblock[1]))
 	{
+		myblock[0] = 'A';
+		myblock[1] = 'B';
+		myblock[2] = convertToFirstChar(4096);
+		myblock[3] = convertToSecondChar(4096);
 	}
+	
+	return NULL;
+}
+
+char * myFree(void * ptr){
 	return NULL;
 }
 
 int main(int argc, char* argv[]){ //test
 	int i = 232;
-	int j = 412408;
+	int j = 4124;
 	struct data dat;
-	dat.test = 41245;
-	//memcpy(&myblock[0],&j,sizeof(int));
-	myblock[0] = dat;
-	printf("%d\n",myblock[0].test);
-	printf("%d\n",(int)myblock[0].test);
+	dat.size = 41245;
+	void * ptr = &myblock[0];
+	*(struct data*)ptr = dat;
+	printf("%d\n",((struct data*)ptr)->size);
 }
