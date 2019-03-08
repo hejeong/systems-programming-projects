5f4e2ff07e93f896d3e020480bb0946ba0f1bc72
Valank <bym7@cd.cs.rutgers.edu>
2019-03-08 02:17:37 -0500

    first rendition of mymalloc

--- 1/mymalloc.c
+++ 1/mymalloc.c
@@ -13,20 +13,14 @@ struct data //metadata, first is the first two digits, second is last two digits
 	char Size2;
 };
 
-void initialize(){
-	struct data meta;
-	meta.Num1 = 'A';
-	meta.Num2 = 'B';
-	meta.Size1 = convertToFirstChar(4096);
-	meta.Size2 = convertToSecondChar(4096);
-	void * ptr = &myblock[0];
-	*(struct data*)ptr = meta;
-}
-
 int compMagic(char first, char second){
-	if((first == 'A' && second == 'B') || (first == 'C' && second == 'D'))
+	if(first == 'U' && second == 'A')
 	{
 		return 1;
+	}
+	else if(first == 'A' && second == 'L')
+	{
+		return 2;
 	}
 	return 0;
 }
@@ -49,28 +43,86 @@ char convertToSecondChar(int num){
 	return c;
 }
 
-void * myMalloc(int size){
+void initialize(){
+	struct data meta;
+	meta.Num1 = 'A';
+	meta.Num2 = 'B';
+	meta.Size1 = convertToFirstChar(4096);
+	meta.Size2 = convertToSecondChar(4096);
+	void * ptr = &myblock[0];
+	*(struct data*)ptr = meta;
+}
+
+void * myMalloc(int size, int file, int line){
 	if(!compMagic(myblock[0], myblock[1]))
 	{
-		myblock[0] = 'A';
-		myblock[1] = 'B';
+		myblock[0] = 'U';
+		myblock[1] = 'A';
 		myblock[2] = convertToFirstChar(4096);
 		myblock[3] = convertToSecondChar(4096);
 	}
 	
+	int i = 0;
+	
+	while(i <= 4093)
+	{
+		int blockSize = convertToSize(myblock[i+2],myblock[i+3]);
+		
+		if(compMagic(myblock[i],myblock[i+1]) == 1 && blockSize >= size)
+		{
+			myblock[i] = 'A';
+			myblock[i+1] = 'L';
+			myblock[i+2] = convertToFirstChar(size);
+			myblock[i+3] = convertToSecondChar(size);
+			i = i + blockSize + 4;
+			myblock[i] = 'U';
+			myblock[i+1] = 'A';
+			myblock[i+2] = convertToFirstChar(blockSize - 4 - size);
+			myblock[i+3] = convertToSecondChar(blockSize - 4 - size);
+			void * ptr = &myblock[i+4];
+			return ptr;
+		}
+		else
+		{
+			i = i + 4 + blockSize;
+		}
+	}
+	//error goes here
 	return NULL;
 }
 
 char * myFree(void * ptr){
+	ptr = ptr - 4;
+	char firstNum = *((char *)ptr);
+	char secondNum = *((char *)(ptr + 1));
+	if(ptr <= &myblock[4092] && ptr >= &myblock[0])
+	{
+		if(compMagic(firstNum, secondNum) == 2)
+		{
+			*((char *)ptr) = 'U';
+			*((char *)(ptr + 1)) = 'A';
+			int blockSize = convertToSize( *((char *)(ptr + 2)) , *((char *)(ptr + 3)) );
+			char secondBlock1 = *((char *)(ptr + 4 + blockSize));
+			char secondBlock2 = *((char *)(ptr + 5 + blockSize));
+			if(compMagic(secondBlock1, secondBlock2) == 2)
+			{
+				blockSize = blockSize + convertToSize(*((char *)(ptr + 6 + blockSize)), *((char *)(ptr + 7 + blockSize)));
+			}
+			*((char *)(ptr + 2)) = convertToFirstChar(blockSize);
+			*((char *)(ptr + 3)) = convertToFirstChar(blockSize);
+		}
+		//error
+	}
+	//error
 	return NULL;
 }
 
 int main(int argc, char* argv[]){ //test
 	int i = 232;
 	int j = 4124;
-	struct data dat;
-	dat.size = 41245;
+	myblock[0] = 'g';
+	myblock[1] = 'j';
 	void * ptr = &myblock[0];
-	*(struct data*)ptr = dat;
-	printf("%d\n",((struct data*)ptr)->size);
+	printf("%c\n", *(char*)ptr);
+	printf("%c\n", *(char*)(ptr+1));
 }
